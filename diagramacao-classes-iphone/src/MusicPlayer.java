d2034c23dc85c408e54d5052cbe7a800e93f9e7d
Jordy Nicholas <56373651+JordyNicholas@users.noreply.github.com>
2023-10-22 22:30:03 -0300

    add desafio-files

---
+++ diagramacao-classes-iphone/src/MusicPlayer.java
@@ -0,0 +1,7 @@
+package src;
+
+public interface MusicPlayer{
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica();
+}
---
+++ diagramacao-classes-iphone/src/iPhone.java
@@ -0,0 +1,50 @@
+package src;
+
+public class iPhone implements MusicPlayer, Celular, Browser {
+
+
+    @Override
+    public void ligar(String numCel) {
+
+    }
+
+    @Override
+    public void atender(String numCel) {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica() {
+
+    }
+}
